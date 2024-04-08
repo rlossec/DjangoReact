78e6768b4a6b140a43e8c1a743f2ec2807f8b62c
Raphael <raphaelossec@gmail.com>
2024-04-08 18:15:11 +0200

    Fix pb with Allowed Host in settings.py when it is empty.

--- src/persistent_data/backend/config/settings.py
+++ src/persistent_data/backend/config/settings.py
@@ -12,7 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG") == "True"
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
+if os.getenv("ALLOWED_HOSTS"):
+    ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
+else:
+    ALLOWED_HOSTS = ['', ]
 
 POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
