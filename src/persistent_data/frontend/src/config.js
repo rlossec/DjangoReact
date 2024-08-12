fef668a0a75e735914ba866cf25a7d634d3b50da
Raphael <raphaelossec@gmail.com>
2024-08-12 22:00:40 +0200

    API Fetch for upload

---
+++ src/persistent_data/frontend/src/config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  API_URL: 'http://localhost:3200/',
+};
+
+export default CONFIG;
--- src/persistent_data/frontend/src/utils/apiFetch.js
+++ src/persistent_data/frontend/src/utils/apiFetch.js
@@ -1,15 +1,21 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3200/'
+import CONFIG from '../config';
+
+const { API_URL } = CONFIG;
 
 
-const apiFetch = async (uri, method='GET', data=null, token=null) => {
+const apiFetch = async (uri, method='GET', data=null, accessToken=null, refreshToken=null) => {
   const url = API_URL + uri
 
-  const headers = {
+  let headers = {
     'Content-Type': 'application/json',
   };
+
+  if (accessToken) {
+    headers['Authorization'] = `Bearer ${accessToken}`;
+  }
 
   const config = {
       method,
@@ -18,17 +24,46 @@ const apiFetch = async (uri, method='GET', data=null, token=null) => {
   };
 
   if (data) {
+    if (data instanceof FormData) {
+      headers['Content-Type'] = 'multipart/form-data';
       config.data = data;
+    } else {
+      headers['Content-Type'] = 'application/json';
+      config.data = data;
+    }
   }
 
-  if (token) {
-    headers['Authorization'] = `Bearer ${token}`;
-    config.headers = headers
-  }
-  console.log(data);
-  const response = await axios(config);
+  try {
 
-  return response
+    const response = await axios(config);
+    return response;
+  } catch (error) {
+    console.log(error);
+    if (error.response.status === 401 && refreshToken) {
+      // Token might have expired, try to refresh it
+      try {
+        const refreshResponse = await axios.post(`${API_URL}dj-rest-auth/token/refresh/`, { refresh: refreshToken });
+
+        if (refreshResponse.status === 200) {
+          const newAccessToken = refreshResponse.data.access;
+          sessionStorage.setItem('accessToken', JSON.stringify(newAccessToken));
+
+          headers['Authorization'] = `Bearer ${newAccessToken}`;
+          config.headers = headers;
+
+          // Retry the original request with the new token
+          const retryResponse = await axios(config);
+          return retryResponse;
+        }
+      } catch (refreshError) {
+        console.error('Unable to refresh token', refreshError);
+        throw refreshError;
+      }
+    } else {
+      console.error('API request error', error);
+      throw error;
+    }
+  }
 };
 
 export default apiFetch;
